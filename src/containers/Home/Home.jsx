7f3b67fd8cfc827655f23c5d004e250042695d1d
Aurel Luciu <ironman@artlogo.ro>
2017-07-10 11:51:49 +0300

    add typography and images

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -7,6 +7,7 @@ import {
 	Pagination,
 	Label,
 	Alert,
+	Image,
 } from 'react-bootstrap';
 import { addStyle } from 'react-bootstrap/lib/utils/bootstrapUtils';
 
@@ -16,6 +17,8 @@ import Checkbox from 'elements/CustomCheckbox/CustomCheckbox';
 import Radio from 'elements/CustomRadio/CustomRadio';
 import ToggleButton from 'elements/ToggleButton/ToggleButton';
 import Slider from 'elements/CustomSlider/CustomSlider';
+
+import avatar from 'img/avatar.jpg';
 
 addStyle(Navbar, 'info', 'primary', 'success', 'transparent', 'danger', 'warning');
 addStyle(Pagination, 'info', 'primary', 'success', 'danger', 'warning');
@@ -742,7 +745,125 @@ class Home extends Component {
 								<strong>Error Alert:</strong> Damn man! You screwed up the server this time. You should find a good excuse for your Boss...
 							</Grid>
 		        </Alert>
+					</div>{/* #notifications */}
+
+
+					<div className="section">
+						<Grid className="tim-container">
+							<div className="title">
+								<h2>Typography</h2>
+							</div>
+
+							<div id="typography">
+								<Row>
+									<div className="tim-typo">
+										<h1><span className="tim-note">Header 1</span>The Life of Material Kit </h1>
+									</div>
+									<div className="tim-typo">
+										<h2><span className="tim-note">Header 2</span>The Life of Material Kit</h2>
+									</div>
+									<div className="tim-typo">
+										<h3><span className="tim-note">Header 3</span>The Life of Material Kit</h3>
+									</div>
+									<div className="tim-typo">
+										<h4><span className="tim-note">Header 4</span>The Life of Material Kit</h4>
+									</div>
+									<div className="tim-typo">
+										<h5><span className="tim-note">Header 5</span>The Life of Material Kit</h5>
+									</div>
+									<div className="tim-typo">
+										<h6><span className="tim-note">Header 6</span>The Life of Material Kit</h6>
+									</div>
+
+									<div className="tim-typo">
+										<p><span className="tim-note">Paragraph</span> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities,
+											to show people, this is the level that things could be at.</p>
+									</div>
+									<div className="tim-typo">
+										<span className="tim-note">Quote</span>
+										<blockquote>
+											<p>
+												I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that
+												things could be at.
+											</p>
+											<small>
+			                         Kanye West, Musician
+			                         </small>
+										</blockquote>
+									</div>
+
+									<div className="tim-typo">
+										<span className="tim-note">Muted Text</span>
+										<p className="text-muted">
+											I will be the leader of a company that ends up being worth billions of dollars, because I got the answers...
+										</p>
+									</div>
+									<div className="tim-typo">
+										<span className="tim-note">Primary Text</span>
+										<p className="text-primary">
+											I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+									</div>
+									<div className="tim-typo">
+										<span className="tim-note">Info Text</span>
+										<p className="text-info">
+											I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+									</div>
+									<div className="tim-typo">
+										<span className="tim-note">Success Text</span>
+										<p className="text-success">
+											I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+									</div>
+									<div className="tim-typo">
+										<span className="tim-note">Warning Text</span>
+										<p className="text-warning">
+											I will be the leader of a company that ends up being worth billions of dollars, because I got the answers...
+										</p>
+									</div>
+									<div className="tim-typo">
+										<span className="tim-note">Danger Text</span>
+										<p className="text-danger">
+											I will be the leader of a company that ends up being worth billions of dollars, because I got the answers... </p>
+									</div>
+									<div className="tim-typo">
+										<h2><span className="tim-note">Small Tag</span>
+										Header with small subtitle <br />
+										<small>Use "small" tag for the headers</small>
+										</h2>
+									</div>
+								</Row>
+							</div>
+
+							<div className="space-50"></div>
+
+							<div id="images">
+								<div className="title">
+									<h2>Images</h2>
+								</div>
+								<br />
+
+								<Row>
+									<Col sm={2}>
+										<h4>Rounded Image</h4>
+										<Image src={avatar} rounded responsive />
+									</Col>
+									<Col sm={2} smOffset={1}>
+										<h4>Circle Image</h4>
+										<Image src={avatar} circle responsive />
+									</Col>
+									<Col sm={2} smOffset={1}>
+										<h4>Rounded Raised</h4>
+										<Image src={avatar} rounded responsive className="img-raised" />
+									</Col>
+
+									<Col sm={2} smOffset={1}>
+										<h4>Circle Raised</h4>
+										<Image src={avatar} circle responsive className="img-raised" />
+									</Col>
+								</Row>
+							</div>{/* #images */}
+						</Grid>
 					</div>
+
 
 	      </div>{/* main */}
 			</div>
