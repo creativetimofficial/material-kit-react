0969a0f29aa37cc3038dc94b08c116911feb44bb
Aurel Luciu <ironman@artlogo.ro>
2017-07-21 18:27:47 +0300

    fix route for Landing Page

--- src/containers/index.js
+++ src/containers/index.js
@@ -5,5 +5,6 @@ import Home from './Home/Home';
 import NotFound from './NotFound/NotFound';
 import ProfileContainer from './Profile/Profile';
 import Signup from './Signup/Signup';
+import Landing from './Landing/Landing';
 
-export { App, Components, Contact, Home, NotFound, ProfileContainer, Signup };
+export { App, Components, Contact, Home, NotFound, ProfileContainer, Signup, Landing };
