e4bb76678ec52827dc951e33506ac6eb6e7929d0
Manu <emanuelnazare@yahoo.ro>
2018-09-29 15:22:39 +0300

    changed version

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Router, Route, Switch } from "react-router-dom";
 
 import indexRoutes from "routes/index.jsx";
 
-import "assets/scss/material-kit-react.css?v=1.2.0";
+import "assets/scss/material-kit-react.css?v=1.3.0";
 
 var hist = createBrowserHistory();
 
