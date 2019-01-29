dd29ff41072c78ab22528b653027846cfedf46bf
Manu <emanuelnazare@yahoo.ro>
2019-01-29 19:14:46 +0200

    - Deleted react-ga

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createHashHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
-import ReactGA from "react-ga";
 import ReactPixel from "react-facebook-pixel";
 
 import indexRoutes from "routes/index.jsx";
@@ -17,13 +16,7 @@ ReactPixel.init("111649226022273");
 ReactPixel.pageView();
 ReactPixel.fbq("track", "PageView");
 
-ReactGA.initialize("UA-46172202-1");
-ReactGA.set({ page: window.location.pathname + window.location.search });
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 hist.listen(location => {
-  ReactGA.set({ page: window.location.pathname + window.location.search });
-  ReactGA.pageview(window.location.pathname + window.location.search);
 
   ReactPixel.pageView();
   ReactPixel.fbq("track", "PageView");
