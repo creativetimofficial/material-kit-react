a3b356ee358b578f32223d3ac3e7c378003aa1fb
Manu <emanuelnazare@yahoo.ro>
2019-02-13 10:02:12 +0200

    - changed file sand folders

--- src/index.js
+++ src/index.js
@@ -3,18 +3,23 @@ import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
-import indexRoutes from "routes/index.jsx";
-
 import "assets/scss/material-kit-react.scss?v=1.4.0";
+
+// pages for this product
+import Components from "views/Components/Components.jsx";
+import LandingPage from "views/LandingPage/LandingPage.jsx";
+import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+import LoginPage from "views/LoginPage/LoginPage.jsx";
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      {indexRoutes.map((prop, key) => {
-        return <Route path={prop.path} key={key} component={prop.component} />;
-      })}
+      <Route path="/landing-page" component={LandingPage} />
+      <Route path="/profile-page" component={ProfilePage} />
+      <Route path="/login-page" component={LoginPage} />
+      <Route path="/" component={Components} />
     </Switch>
   </Router>,
   document.getElementById("root")
