8d344458a4afca795f1c74fdb8fbfb252162fd16
carmenmitru <carmen.mitru@gmail.com>
2019-02-25 11:18:34 +0200

    add testing

---
+++ tests/components.spec.js
@@ -0,0 +1,47 @@
+const fetch = require("node-fetch");
+describe("Components Page", function() {
+  beforeEach(function() {
+    browser.url("./");
+    browser.pause(5000)
+  });
+  it("renders Components page", function() {
+    var results = browser.checkDocument();
+
+    results.forEach(function(result) {
+      expect(result.isWithinMisMatchTolerance).to.equal(
+        true,
+        "screenshot failure"
+      );
+    });
+  });
+  it("check the page for broken links", async function() {
+    const links = browser.$$("a");
+
+    let urls = links.map(link => link.getAttribute("href"));
+
+    const isValidUrl = url => {
+      const regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+      return regexp.test(url);
+    };
+
+    const validUrls = urls.filter(isValidUrl);
+
+    const requests = validUrls.map(url => fetch(url));
+
+    const responses = await Promise.all(requests);
+
+    const statusCodes = responses.map(response => response.status);
+    if (statusCodes.includes(404)) {
+      console.log(
+        "============== Components Page Broken links ==================="
+      );
+      statusCodes.forEach((statusCode, i) => {
+        if (statusCode == 404 && validUrls[i].indexOf("index.html") === -1) {
+          console.log("Invalid url " + validUrls[i]);
+        }
+      });
+      var statusCodeExpeced = new Array(statusCodes.length).fill(200);
+      expect(statusCodes).to.deep.equal(statusCodeExpeced);
+    }
+  });
+});
