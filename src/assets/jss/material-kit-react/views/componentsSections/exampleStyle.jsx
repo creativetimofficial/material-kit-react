a7e9b38bbd14893a248dc366337684c304b74375
Alex Mueller <alex@sporkbytes.com>
2019-04-16 18:01:41 -0700

    Fixes typo where containerFluid was spelled conatinerFluid.

--- src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.jsx
+++ src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.jsx
@@ -1,4 +1,4 @@
-import { conatinerFluid } from "assets/jss/material-kit-react.jsx";
+import { containerFluid } from "assets/jss/material-kit-react.jsx";
 
 import imagesStyle from "assets/jss/material-kit-react/imagesStyles.jsx";
 
@@ -7,7 +7,7 @@ const exampleStyle = {
     padding: "70px 0"
   },
   container: {
-    ...conatinerFluid,
+    ...containerFluid,
     textAlign: "center !important"
   },
   ...imagesStyle,
