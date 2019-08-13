c06d8522ff1d78c6fbbc8e7fcdf4ea4ce35c4ac4
EINazare <emanuelioannazare@gmail.com>
2019-08-13 15:50:23 +0300

    - Clearfix

--- src/views/Documentation/Sections/Clearfix.jsx
+++ src/views/Documentation/Sections/Clearfix.jsx
@@ -22,30 +22,26 @@ import { prism } from "react-syntax-highlighter/dist/styles/prism";
 const codeImport = `import Clearfix from "components/Clearfix/Clearfix.js";`;
 const codeExample = `<Clearfix />`;
 
-class ClearExample extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Clearfix</h1>
-        <p>
-          A clearfix is a way for an element to automatically clear its child
-          elements, so that you don't need to add additional markup. It's
-          generally used in float layouts where elements are floated to be
-          stacked horizontally.
-        </p>
-        <p>
-          You can see it being used in{" "}
-          <code>src/views/Components/Sections/SectionNotifications.jsx</code>.
-        </p>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeImport}
-        </SyntaxHighlighter>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExample}
-        </SyntaxHighlighter>
-      </div>
-    );
-  }
+export default function ClearExample() {
+  return (
+    <div>
+      <h1>Clearfix</h1>
+      <p>
+        A clearfix is a way for an element to automatically clear its child
+        elements, so that you don't need to add additional markup. It's
+        generally used in float layouts where elements are floated to be stacked
+        horizontally.
+      </p>
+      <p>
+        You can see it being used in{" "}
+        <code>src/views/Components/Sections/SectionNotifications.jsx</code>.
+      </p>
+      <SyntaxHighlighter language="jsx" style={prism}>
+        {codeImport}
+      </SyntaxHighlighter>
+      <SyntaxHighlighter language="jsx" style={prism}>
+        {codeExample}
+      </SyntaxHighlighter>
+    </div>
+  );
 }
-
-export default ClearExample;
