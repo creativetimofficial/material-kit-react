c38744d55ac4c0ff32f29d3f3111eccc57c1f401
EINazare <emanuelioannazare@gmail.com>
2019-08-13 15:52:33 +0300

    - Datetimepicker hooks

--- src/views/Documentation/Sections/DateTimePicker.jsx
+++ src/views/Documentation/Sections/DateTimePicker.jsx
@@ -1,27 +1,10 @@
-/*!
-
-=========================================================
-* Material Kit React - v1.7.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-kit-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { prism } from "react-syntax-highlighter/dist/styles/prism";
 // react component plugin for creating a beautiful datetime dropdown picker
 import Datetime from "react-datetime";
 // material-ui components
-import withStyles from "@material-ui/core/styles/withStyles";
+import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 // @material-ui/icons
@@ -49,11 +32,13 @@ const styles = {
   }
 };
 
+const useStyles = makeStyles(styles);
+
 const codeExample = `import React from 'react';
 // react component plugin for creating a beautiful datetime dropdown picker
 import Datetime from "react-datetime";
 // material-ui components
-import withStyles from "@material-ui/core/styles/withStyles";
+import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
 import FormControl from "@material-ui/core/FormControl";
 // @material-ui/icons
@@ -71,84 +56,76 @@ const styles = {
   },
 };
 
-class DateTimePicker extends React.Component{
-  render(){
-    const { classes } = this.props;
-    return (
-      <div>
-        <InputLabel className={classes.label}>
-          Datetime Picker
-        </InputLabel>
+const useStyles = makeStyles(styles);
+
+export default function DateTimePicker(){
+  const classes = useStyles();
+  return (
+    <div>
+      <InputLabel className={classes.label}>
+        Datetime Picker
+      </InputLabel>
+      <br />
+      <FormControl fullWidth>
+        <Datetime
+          inputProps={{ placeholder: "Datetime Picker Here" }}
+        />
+      </FormControl>
+    </div>
+  );
+}`;
+
+export default function DateTimePicker() {
+  const classes = useStyles();
+  return (
+    <div>
+      <h1>React Datetime v2.16.3</h1>
+      <p>
+        We've used{" "}
+        <a
+          href="https://github.com/YouCanBookMe/react-datetime?ref=creativetim"
+          target="_blank"
+        >
+          react-datetime
+        </a>{" "}
+        for this component and we've restyled it to match our theme.
+      </p>
+      <p>
+        Please refer to{" "}
+        <a
+          href="https://github.com/YouCanBookMe/react-datetime?ref=creativetim"
+          target="_blank"
+        >
+          react-datetime documentation
+        </a>{" "}
+        for more information.
+      </p>
+      <h2>Styles</h2>
+      <p>
+        You will find the styles for this component in
+        <br /> <code>src/assets/scss/plugins/_plugin-react-datetime.scss</code>.
+      </p>
+      <p>
+        If you want to match the input of this plugin with the inputs from
+        Material-UI, please take a look at this issue here:{" "}
+        <a
+          href="https://github.com/creativetimofficial/material-kit-react/issues/20?ref=creativetim"
+          target="_blank"
+        >
+          https://github.com/creativetimofficial/material-kit-react/issues/20
+        </a>
+      </p>
+      <h2>Example</h2>
+      <div className={classes.bdExample}>
+        <InputLabel className={classes.label}>Datetime Picker</InputLabel>
         <br />
         <FormControl fullWidth>
-          <Datetime
-            inputProps={{ placeholder: "Datetime Picker Here" }}
-          />
+          <Datetime inputProps={{ placeholder: "Datetime Picker Here" }} />
         </FormControl>
       </div>
-    );
-  }
+      <SyntaxHighlighter language="jsx" style={prism}>
+        {codeExample}
+      </SyntaxHighlighter>
+    </div>
+  );
 }
-
-export default withStyles(styles)(DateTimePicker);
-`;
-
-class DateTimePicker extends React.Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div>
-        <h1>React Datetime v2.16.3</h1>
-        <p>
-          We've used{" "}
-          <a
-            href="https://github.com/YouCanBookMe/react-datetime?ref=creativetim"
-            target="_blank"
-          >
-            react-datetime
-          </a>{" "}
-          for this component and we've restyled it to match our theme.
-        </p>
-        <p>
-          Please refer to{" "}
-          <a
-            href="https://github.com/YouCanBookMe/react-datetime?ref=creativetim"
-            target="_blank"
-          >
-            react-datetime documentation
-          </a>{" "}
-          for more information.
-        </p>
-        <h2>Styles</h2>
-        <p>
-          You will find the styles for this component in
-          <br />{" "}
-          <code>src/assets/scss/plugins/_plugin-react-datetime.scss</code>.
-        </p>
-        <p>
-          If you want to match the input of this plugin with the inputs from
-          Material-UI, please take a look at this issue here:{" "}
-          <a
-            href="https://github.com/creativetimofficial/material-kit-react/issues/20?ref=creativetim"
-            target="_blank"
-          >
-            https://github.com/creativetimofficial/material-kit-react/issues/20
-          </a>
-        </p>
-        <h2>Example</h2>
-        <div className={classes.bdExample}>
-          <InputLabel className={classes.label}>Datetime Picker</InputLabel>
-          <br />
-          <FormControl fullWidth>
-            <Datetime inputProps={{ placeholder: "Datetime Picker Here" }} />
-          </FormControl>
-        </div>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExample}
-        </SyntaxHighlighter>
-      </div>
-    );
-  }
-}
-
-export default withStyles(styles)(DateTimePicker);
