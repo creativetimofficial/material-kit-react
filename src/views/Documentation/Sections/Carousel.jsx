50af8c739b1a98219187d57d54feec3b66ca1fb5
EINazare <emanuelioannazare@gmail.com>
2019-08-13 15:33:30 +0300

    - Carousel hooks

--- src/views/Documentation/Sections/Carousel.jsx
+++ src/views/Documentation/Sections/Carousel.jsx
@@ -21,7 +21,7 @@ import { prism } from "react-syntax-highlighter/dist/styles/prism";
 // react component for creating beautiful carousel
 import Carousel from "react-slick";
 // material-ui components
-import withStyles from "@material-ui/core/styles/withStyles";
+import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
 import LocationOn from "@material-ui/icons/LocationOn";
 // core components
@@ -61,173 +61,159 @@ import image1 from "assets/img/bg.jpg";
 import image2 from "assets/img/bg2.jpg";
 import image3 from "assets/img/bg3.jpg";
 
-class SectionCarousel extends React.Component {
-  render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true
-    };
-    return (
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={8}>
-              <Card>
-                <Carousel {...settings}>
-                  <div>
-                    <img
-                      src={image1}
-                      alt="First slide"
-                      className="slick-image"
-                    />
-                    <div className="slick-caption">
-                      <h4>
-                        <LocationOn className="slick-icons" />Yellowstone
-                        National Park, United States
-                      </h4>
-                    </div>
-                  </div>
-                  <div>
-                    <img
-                      src={image2}
-                      alt="Second slide"
-                      className="slick-image"
-                    />
-                    <div className="slick-caption">
-                      <h4>
-                        <LocationOn className="slick-icons" />Somewhere Beyond,
-                        United States
-                      </h4>
-                    </div>
-                  </div>
-                  <div>
-                    <img
-                      src={image3}
-                      alt="Third slide"
-                      className="slick-image"
-                    />
-                    <div className="slick-caption">
-                      <h4>
-                        <LocationOn className="slick-icons" />Yellowstone
-                        National Park, United States
-                      </h4>
-                    </div>
-                  </div>
-                </Carousel>
-              </Card>
-            </GridItem>
-          </GridContainer>
-    );
-  }
-}
+export default function SectionCarousel(){
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  };
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={8}>
+        <Card>
+          <Carousel {...settings}>
+            <div>
+              <img
+                src={image1}
+                alt="First slide"
+                className="slick-image"
+              />
+              <div className="slick-caption">
+                <h4>
+                  <LocationOn className="slick-icons" />Yellowstone
+                  National Park, United States
+                </h4>
+              </div>
+            </div>
+            <div>
+              <img
+                src={image2}
+                alt="Second slide"
+                className="slick-image"
+              />
+              <div className="slick-caption">
+                <h4>
+                  <LocationOn className="slick-icons" />Somewhere Beyond,
+                  United States
+                </h4>
+              </div>
+            </div>
+            <div>
+              <img
+                src={image3}
+                alt="Third slide"
+                className="slick-image"
+              />
+              <div className="slick-caption">
+                <h4>
+                  <LocationOn className="slick-icons" />Yellowstone
+                  National Park, United States
+                </h4>
+              </div>
+            </div>
+          </Carousel>
+        </Card>
+      </GridItem>
+    </GridContainer>
+  );
+}`;
 
-export default SectionCarousel;`;
+const useStyles = makeStyles(styles);
 
-class SectionCarousel extends React.Component {
-  render() {
-    const { classes } = this.props;
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true
-    };
-    return (
-      <div>
-        <h1>React Slick 0.24.0</h1>
-        <h2>Styles</h2>
-        <p>
-          You will find the styles for this component in
-          <br />
-          <code>src/assets/scss/plugins/_plugin-react-slick.scss</code>.
-        </p>
-        <h2>Example</h2>
-        <div className={classes.bdExample}>
-          <GridContainer>
-            <GridItem xs={12} sm={12} md={8}>
-              <Card>
-                <Carousel {...settings}>
-                  <div>
-                    <img
-                      src={image1}
-                      alt="First slide"
-                      className="slick-image"
-                    />
-                    <div className="slick-caption">
-                      <h4>
-                        <LocationOn className="slick-icons" />
-                        Yellowstone National Park, United States
-                      </h4>
-                    </div>
+export default function SectionCarousel() {
+  const classes = useStyles();
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true
+  };
+  return (
+    <div>
+      <h1>React Slick 0.24.0</h1>
+      <h2>Styles</h2>
+      <p>
+        You will find the styles for this component in
+        <br />
+        <code>src/assets/scss/plugins/_plugin-react-slick.scss</code>.
+      </p>
+      <h2>Example</h2>
+      <div className={classes.bdExample}>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8}>
+            <Card>
+              <Carousel {...settings}>
+                <div>
+                  <img src={image1} alt="First slide" className="slick-image" />
+                  <div className="slick-caption">
+                    <h4>
+                      <LocationOn className="slick-icons" />
+                      Yellowstone National Park, United States
+                    </h4>
                   </div>
-                  <div>
-                    <img
-                      src={image2}
-                      alt="Second slide"
-                      className="slick-image"
-                    />
-                    <div className="slick-caption">
-                      <h4>
-                        <LocationOn className="slick-icons" />
-                        Somewhere Beyond, United States
-                      </h4>
-                    </div>
+                </div>
+                <div>
+                  <img
+                    src={image2}
+                    alt="Second slide"
+                    className="slick-image"
+                  />
+                  <div className="slick-caption">
+                    <h4>
+                      <LocationOn className="slick-icons" />
+                      Somewhere Beyond, United States
+                    </h4>
                   </div>
-                  <div>
-                    <img
-                      src={image3}
-                      alt="Third slide"
-                      className="slick-image"
-                    />
-                    <div className="slick-caption">
-                      <h4>
-                        <LocationOn className="slick-icons" />
-                        Yellowstone National Park, United States
-                      </h4>
-                    </div>
+                </div>
+                <div>
+                  <img src={image3} alt="Third slide" className="slick-image" />
+                  <div className="slick-caption">
+                    <h4>
+                      <LocationOn className="slick-icons" />
+                      Yellowstone National Park, United States
+                    </h4>
                   </div>
-                </Carousel>
-              </Card>
-            </GridItem>
-          </GridContainer>
-        </div>
-        <SyntaxHighlighter language="jsx" style={prism}>
-          {codeExample}
-        </SyntaxHighlighter>
-        <h2>Props</h2>
-        <p>
-          We've added some extra classes to the default ones from{" "}
-          <a
-            href="https://react-slick.neostack.com/?ref=creativetim"
-            target="_blank"
-          >
-            react-slick
-          </a>
-          .
-        </p>
-        <p>
-          These are <code>.slick-image</code> for the image of the slide,{" "}
-          <code>.slick-caption</code>, the text that appears over the image and{" "}
-          <code>.slick-icons</code>, used for the icon that goes along with the
-          text.
-        </p>
-        <p>
-          For more details please refer to{" "}
-          <a
-            href="https://react-slick.neostack.com/docs/get-started?ref=creativetim"
-            target="_blank"
-          >
-            react-slick's official docs
-          </a>
-          .
-        </p>
+                </div>
+              </Carousel>
+            </Card>
+          </GridItem>
+        </GridContainer>
       </div>
-    );
-  }
+      <SyntaxHighlighter language="jsx" style={prism}>
+        {codeExample}
+      </SyntaxHighlighter>
+      <h2>Props</h2>
+      <p>
+        We've added some extra classes to the default ones from{" "}
+        <a
+          href="https://react-slick.neostack.com/?ref=creativetim"
+          target="_blank"
+        >
+          react-slick
+        </a>
+        .
+      </p>
+      <p>
+        These are <code>.slick-image</code> for the image of the slide,{" "}
+        <code>.slick-caption</code>, the text that appears over the image and{" "}
+        <code>.slick-icons</code>, used for the icon that goes along with the
+        text.
+      </p>
+      <p>
+        For more details please refer to{" "}
+        <a
+          href="https://react-slick.neostack.com/docs/get-started?ref=creativetim"
+          target="_blank"
+        >
+          react-slick's official docs
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
-
-export default withStyles(styles)(SectionCarousel);
