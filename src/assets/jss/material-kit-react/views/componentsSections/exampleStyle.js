3229cdca491503797df6afc074b911b193ccf722
EINazare <emanuelioannazare@gmail.com>
2020-05-28 15:39:31 +0300

    fix: solve issue after merging PR

--- src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.js
+++ src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.js
@@ -1,4 +1,4 @@
-import { containerFluid } from "assets/jss/material-kit-react.jsx";
+import { containerFluid } from "assets/jss/material-kit-react.js";
 
 import imagesStyle from "assets/jss/material-kit-react/imagesStyles.js";
 
