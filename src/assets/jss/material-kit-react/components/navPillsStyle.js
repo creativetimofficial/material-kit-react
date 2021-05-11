1778693e34122f5c9e45a2e385bb9dbfe633ed1a
EINazare <emanuelioannazare@gmail.com>
2021-05-11 10:23:17 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/components/navPillsStyle.js
+++ src/assets/jss/material-kit-react/components/navPillsStyle.js
@@ -4,10 +4,10 @@ import {
   infoColor,
   successColor,
   warningColor,
-  dangerColor
+  dangerColor,
 } from "assets/jss/material-kit-react.js";
 
-const navPillsStyle = theme => ({
+const navPillsStyle = (theme) => ({
   root: {
     marginTop: "20px",
     paddingLeft: "0",
@@ -19,22 +19,22 @@ const navPillsStyle = theme => ({
     fontWeight: "500",
     position: "relative",
     display: "block",
-    color: "inherit"
+    color: "inherit",
   },
   flexContainer: {
     [theme.breakpoints.down("xs")]: {
       display: "flex",
-      flexWrap: "wrap"
-    }
+      flexWrap: "wrap",
+    },
   },
   displayNone: {
-    display: "none !important"
+    display: "none !important",
   },
   fixed: {
-    overflow: "visible !important"
+    overflow: "visible !important",
   },
   horizontalDisplay: {
-    display: "block"
+    display: "block",
   },
   pills: {
     float: "left",
@@ -49,10 +49,10 @@ const navPillsStyle = theme => ({
     height: "auto",
     opacity: "1",
     maxWidth: "100%",
-    margin: "0 5px"
+    margin: "0 5px",
   },
   pillsWithIcons: {
-    borderRadius: "4px"
+    borderRadius: "4px",
   },
   tabIcon: {
     width: "30px",
@@ -60,70 +60,70 @@ const navPillsStyle = theme => ({
     display: "block",
     margin: "15px 0 !important",
     "&, & *": {
-      letterSpacing: "normal !important"
-    }
+      letterSpacing: "normal !important",
+    },
   },
   horizontalPills: {
     width: "100%",
     float: "none !important",
     "& + button": {
-      margin: "10px 0"
-    }
+      margin: "10px 0",
+    },
   },
   contentWrapper: {
-    marginTop: "20px"
+    marginTop: "20px",
   },
   primary: {
     "&,&:hover": {
       color: "#FFFFFF",
       backgroundColor: primaryColor,
       boxShadow:
-        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(156, 39, 176, 0.4)"
-    }
+        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(156, 39, 176, 0.4)",
+    },
   },
   info: {
     "&,&:hover": {
       color: "#FFFFFF",
       backgroundColor: infoColor,
       boxShadow:
-        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(76, 175, 80, 0.4)"
-    }
+        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(76, 175, 80, 0.4)",
+    },
   },
   success: {
     "&,&:hover": {
       color: "#FFFFFF",
       backgroundColor: successColor,
       boxShadow:
-        "0 2px 2px 0 rgba(76, 175, 80, 0.14), 0 3px 1px -2px rgba(76, 175, 80, 0.2), 0 1px 5px 0 rgba(76, 175, 80, 0.12)"
-    }
+        "0 2px 2px 0 rgba(76, 175, 80, 0.14), 0 3px 1px -2px rgba(76, 175, 80, 0.2), 0 1px 5px 0 rgba(76, 175, 80, 0.12)",
+    },
   },
   warning: {
     "&,&:hover": {
       color: "#FFFFFF",
       backgroundColor: warningColor,
       boxShadow:
-        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(255, 152, 0, 0.4)"
-    }
+        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(255, 152, 0, 0.4)",
+    },
   },
   danger: {
     "&,&:hover": {
       color: "#FFFFFF",
       backgroundColor: dangerColor,
       boxShadow:
-        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(255, 152, 0, 0.4)"
-    }
+        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(255, 152, 0, 0.4)",
+    },
   },
   rose: {
     "&,&:hover": {
       color: "#FFFFFF",
       backgroundColor: roseColor,
       boxShadow:
-        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(233, 30, 99, 0.4)"
-    }
+        "0 4px 20px 0px rgba(0, 0, 0, 0.14), 0 7px 10px -5px rgba(233, 30, 99, 0.4)",
+    },
   },
   alignCenter: {
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
   tabWrapper: {
     color: "inherit",
@@ -133,9 +133,9 @@ const navPillsStyle = theme => ({
     fontWeight: "500",
     textTransform: "uppercase",
     "&,& *": {
-      letterSpacing: "normal"
-    }
-  }
+      letterSpacing: "normal",
+    },
+  },
 });
 
 export default navPillsStyle;
