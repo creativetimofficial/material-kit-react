caf33a8c22eacaa6627cf70c21c8ffe9d8744074
EINazare <emanuelioannazare@gmail.com>
2021-05-11 10:23:19 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/views/profilePage.js
+++ src/assets/jss/material-kit-react/views/profilePage.js
@@ -10,29 +10,29 @@ const profilePageStyle = {
       maxWidth: "160px",
       width: "100%",
       margin: "0 auto",
-      transform: "translate3d(0, -50%, 0)"
-    }
+      transform: "translate3d(0, -50%, 0)",
+    },
   },
   description: {
     margin: "1.071rem auto 0",
     maxWidth: "600px",
     color: "#999",
-    textAlign: "center !important"
+    textAlign: "center !important",
   },
   name: {
-    marginTop: "-80px"
+    marginTop: "-80px",
   },
   ...imagesStyle,
   main: {
     background: "#FFFFFF",
     position: "relative",
-    zIndex: "3"
+    zIndex: "3",
   },
   mainRaised: {
     margin: "-60px 30px 0px",
     borderRadius: "6px",
     boxShadow:
-      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+      "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)",
   },
   title: {
     ...title,
@@ -40,7 +40,7 @@ const profilePageStyle = {
     position: "relative",
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
+    textDecoration: "none",
   },
   socials: {
     marginTop: "0",
@@ -51,12 +51,12 @@ const profilePageStyle = {
     height: "100%",
     lineHeight: "41px",
     fontSize: "20px",
-    color: "#999"
+    color: "#999",
   },
   navWrapper: {
     margin: "20px auto 50px auto",
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 };
 
 export default profilePageStyle;
