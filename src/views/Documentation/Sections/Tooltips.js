43ec9eaac8c1515e91c32417bbcdacb4c732b3c6
EINazare <emanuelioannazare@gmail.com>
2021-05-11 11:36:21 +0300

    style: run prettier

--- src/views/Documentation/Sections/Tooltips.js
+++ src/views/Documentation/Sections/Tooltips.js
@@ -21,8 +21,8 @@ const styles = {
     marginBottom: "-8px",
     marginLeft: "0",
     borderWidth: ".2rem",
-    border: "solid rgb(245, 242, 240)"
-  }
+    border: "solid rgb(245, 242, 240)",
+  },
 };
 
 const useStyles = makeStyles(styles);
