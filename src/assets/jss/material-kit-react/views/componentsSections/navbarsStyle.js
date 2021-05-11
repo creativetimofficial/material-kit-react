14e0cd8db2150b6679172f419b3233bd3686ddf7
EINazare <emanuelioannazare@gmail.com>
2021-05-11 10:23:18 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/views/componentsSections/navbarsStyle.js
+++ src/assets/jss/material-kit-react/views/componentsSections/navbarsStyle.js
@@ -1,17 +1,17 @@
 import { container, title } from "assets/jss/material-kit-react.js";
 import headerLinksStyle from "assets/jss/material-kit-react/components/headerLinksStyle.js";
 
-const navbarsStyle = theme => ({
+const navbarsStyle = (theme) => ({
   section: {
     padding: "70px 0",
-    paddingTop: "0"
+    paddingTop: "0",
   },
   container,
   title: {
     ...title,
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
+    textDecoration: "none",
   },
   navbar: {
     marginBottom: "-20px",
@@ -19,39 +19,39 @@ const navbarsStyle = theme => ({
     position: "relative",
     overflow: "hidden",
     "& header": {
-      borderRadius: "0"
-    }
+      borderRadius: "0",
+    },
   },
   navigation: {
     backgroundPosition: "center center",
     backgroundSize: "cover",
     marginTop: "0",
-    minHeight: "740px"
+    minHeight: "740px",
   },
   formControl: {
     margin: "0 !important",
-    paddingTop: "0"
+    paddingTop: "0",
   },
   inputRootCustomClasses: {
-    margin: "0!important"
+    margin: "0!important",
   },
   searchIcon: {
     width: "20px",
     height: "20px",
-    color: "inherit"
+    color: "inherit",
   },
   ...headerLinksStyle(theme),
   img: {
     width: "40px",
     height: "40px",
-    borderRadius: "50%"
+    borderRadius: "50%",
   },
   imageDropdownButton: {
     padding: "0px",
     top: "4px",
     borderRadius: "50%",
-    marginLeft: "5px"
-  }
+    marginLeft: "5px",
+  },
 });
 
 export default navbarsStyle;
