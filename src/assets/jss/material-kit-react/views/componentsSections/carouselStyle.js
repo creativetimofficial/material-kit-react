5e1e5d9aed5e0a4678b2c9fb1ee6a1295103a071
EINazare <emanuelioannazare@gmail.com>
2021-05-11 10:23:18 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/views/componentsSections/carouselStyle.js
+++ src/assets/jss/material-kit-react/views/componentsSections/carouselStyle.js
@@ -2,13 +2,13 @@ import { container } from "assets/jss/material-kit-react.js";
 
 const carouselStyle = {
   section: {
-    padding: "70px 0"
+    padding: "70px 0",
   },
   container,
   marginAuto: {
     marginLeft: "auto !important",
-    marginRight: "auto !important"
-  }
+    marginRight: "auto !important",
+  },
 };
 
 export default carouselStyle;
