10b13c249ec2238c49cad6085cbfa0cedbb20a8b
EINazare <emanuelioannazare@gmail.com>
2021-05-11 11:36:17 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/components/cardStyle.js
+++ src/assets/jss/material-kit-react/components/cardStyle.js
@@ -15,15 +15,15 @@ const cardStyle = {
     minWidth: "0",
     wordWrap: "break-word",
     fontSize: ".875rem",
-    transition: "all 300ms linear"
+    transition: "all 300ms linear",
   },
   cardPlain: {
     background: "transparent",
-    boxShadow: "none"
+    boxShadow: "none",
   },
   cardCarousel: {
-    overflow: "hidden"
-  }
+    overflow: "hidden",
+  },
 };
 
 export default cardStyle;
