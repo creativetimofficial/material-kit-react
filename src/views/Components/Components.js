7a08c38523136192a8b75264f7e7cf61c8de3b9e
EINazare <emanuelioannazare@gmail.com>
2021-05-11 11:36:20 +0300

    style: run prettier

--- src/views/Components/Components.js
+++ src/views/Components/Components.js
@@ -44,7 +44,7 @@ export default function Components(props) {
         color="transparent"
         changeColorOnScroll={{
           height: 400,
-          color: "white"
+          color: "white",
         }}
         {...rest}
       />
