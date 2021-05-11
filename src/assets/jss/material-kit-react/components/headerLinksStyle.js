f8f204199c3f6abfd9d56e9a78d1b1040b5a84ec
EINazare <emanuelioannazare@gmail.com>
2021-05-11 10:23:17 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/components/headerLinksStyle.js
+++ src/assets/jss/material-kit-react/components/headerLinksStyle.js
@@ -2,7 +2,7 @@ import { defaultFont } from "assets/jss/material-kit-react.js";
 
 import tooltip from "assets/jss/material-kit-react/tooltipsStyle.js";
 
-const headerLinksStyle = theme => ({
+const headerLinksStyle = (theme) => ({
   list: {
     ...defaultFont,
     fontSize: "14px",
@@ -11,7 +11,7 @@ const headerLinksStyle = theme => ({
     listStyle: "none",
     paddingTop: "0",
     paddingBottom: "0",
-    color: "inherit"
+    color: "inherit",
   },
   listItem: {
     float: "left",
@@ -29,12 +29,12 @@ const headerLinksStyle = theme => ({
         display: "block",
         height: "1px",
         marginLeft: "15px",
-        backgroundColor: "#e5e5e5"
-      }
-    }
+        backgroundColor: "#e5e5e5",
+      },
+    },
   },
   listItemText: {
-    padding: "0 !important"
+    padding: "0 !important",
   },
   navLink: {
     color: "inherit",
@@ -50,7 +50,7 @@ const headerLinksStyle = theme => ({
     display: "inline-flex",
     "&:hover,&:focus": {
       color: "inherit",
-      background: "rgba(200, 200, 200, 0.2)"
+      background: "rgba(200, 200, 200, 0.2)",
     },
     [theme.breakpoints.down("sm")]: {
       width: "calc(100% - 30px)",
@@ -59,9 +59,9 @@ const headerLinksStyle = theme => ({
       marginTop: "8px",
       textAlign: "left",
       "& > span:first-child": {
-        justifyContent: "flex-start"
-      }
-    }
+        justifyContent: "flex-start",
+      },
+    },
   },
   notificationNavLink: {
     color: "inherit",
@@ -73,7 +73,7 @@ const headerLinksStyle = theme => ({
     textDecoration: "none",
     margin: "0px",
     display: "inline-flex",
-    top: "4px"
+    top: "4px",
   },
   registerNavLink: {
     top: "3px",
@@ -84,34 +84,34 @@ const headerLinksStyle = theme => ({
     lineHeight: "20px",
     textDecoration: "none",
     margin: "0px",
-    display: "inline-flex"
+    display: "inline-flex",
   },
   navLinkActive: {
     color: "inherit",
-    backgroundColor: "rgba(255, 255, 255, 0.1)"
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
   },
   icons: {
     width: "20px",
     height: "20px",
-    marginRight: "3px"
+    marginRight: "3px",
   },
   socialIcons: {
     position: "relative",
     fontSize: "20px !important",
-    marginRight: "4px"
+    marginRight: "4px",
   },
   dropdownLink: {
     "&,&:hover,&:focus": {
       color: "inherit",
       textDecoration: "none",
       display: "block",
-      padding: "10px 20px"
-    }
+      padding: "10px 20px",
+    },
   },
   ...tooltip,
   marginRight5: {
-    marginRight: "5px"
-  }
+    marginRight: "5px",
+  },
 });
 
 export default headerLinksStyle;
