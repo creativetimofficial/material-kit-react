102cf41603f73c1b0fce10d9677fad00b99f3a04
EINazare <emanuelioannazare@gmail.com>
2021-05-11 10:23:18 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/views/componentsSections/pillsStyle.js
+++ src/assets/jss/material-kit-react/views/componentsSections/pillsStyle.js
@@ -2,15 +2,15 @@ import { container, title } from "assets/jss/material-kit-react.js";
 
 const pillsStyle = {
   section: {
-    padding: "70px 0"
+    padding: "70px 0",
   },
   container,
   title: {
     ...title,
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
-  }
+    textDecoration: "none",
+  },
 };
 
 export default pillsStyle;
