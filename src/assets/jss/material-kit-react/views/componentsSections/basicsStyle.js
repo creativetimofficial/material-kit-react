78a607580709861af9cc45a1a968beddb791c6d1
EINazare <emanuelioannazare@gmail.com>
2021-05-11 11:36:18 +0300

    style: run prettier

--- src/assets/jss/material-kit-react/views/componentsSections/basicsStyle.js
+++ src/assets/jss/material-kit-react/views/componentsSections/basicsStyle.js
@@ -3,29 +3,29 @@ import customCheckboxRadioSwitch from "assets/jss/material-kit-react/customCheck
 
 const basicsStyle = {
   sections: {
-    padding: "70px 0"
+    padding: "70px 0",
   },
   container,
   title: {
     ...title,
     marginTop: "30px",
     minHeight: "32px",
-    textDecoration: "none"
+    textDecoration: "none",
   },
   space50: {
     height: "50px",
-    display: "block"
+    display: "block",
   },
   space70: {
     height: "70px",
-    display: "block"
+    display: "block",
   },
   icons: {
     width: "17px",
     height: "17px",
-    color: "#FFFFFF"
+    color: "#FFFFFF",
   },
-  ...customCheckboxRadioSwitch
+  ...customCheckboxRadioSwitch,
 };
 
 export default basicsStyle;
