3c1c2fcc8c9a10328a9675c321fc669b17111796
Guilherme Rogatto <gui.rogatto@gmail.com>
2022-10-05 15:07:57 -0300

    removing page

--- src/routes.js
+++ src/routes.js
@@ -44,7 +44,6 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 
 // Pages
 import AboutUs from "layouts/pages/landing-pages/about-us";
-import ContactUs from "layouts/pages/landing-pages/contact-us";
 import Author from "layouts/pages/landing-pages/author";
 import SignIn from "layouts/pages/authentication/sign-in";
 
@@ -82,11 +81,6 @@ const routes = [
             name: "about us",
             route: "/pages/landing-pages/about-us",
             component: <AboutUs />,
-          },
-          {
-            name: "contact us",
-            route: "/pages/landing-pages/contact-us",
-            component: <ContactUs />,
           },
           {
             name: "author",
