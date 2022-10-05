744dc1f5663e380260815a32f4f68d847f1c2630
Guilherme Rogatto <gui.rogatto@gmail.com>
2022-10-05 14:25:26 -0300

    more configs

--- .next/static/chunks/pages/LandingPages/Author-ea69124810b5991a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[922],{88837:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/LandingPages/Author",function(){return u(65217)}])}},function(n){n.O(0,[774,373,539,84,178,824,338,389,890,265,320,601,888,179],(function(){return _=88837,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
--- .next/static/chunks/pages/LandingPages/ContactUs-d438f85f8df6f46c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[388],{61181:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/LandingPages/ContactUs",function(){return u(59833)}])}},function(n){n.O(0,[774,373,539,84,178,824,338,389,890,265,320,601,888,179],(function(){return _=61181,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
