4a02990267c1eb978dc063866d332728e6235b05
joan <joan.arce@jalasoft.com>
2023-02-14 17:57:27 -0400

    Fix: ReactDOM.render is no longer supported in React 18

--- src/index.js
+++ src/index.js
@@ -14,13 +14,14 @@ Coded by www.creative-tim.com
 */
 
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
