5450bcf4799c58157221e3e6e673670ddd2db4d6
Maisam Afshar <maisam.afshar@gmail.com>
2023-04-10 16:34:22 +0430

    refactor ReactDom with ReactDOMClient for react 18

--- src/index.js
+++ src/index.js
@@ -14,13 +14,17 @@ Coded by www.creative-tim.com
 */
 
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
