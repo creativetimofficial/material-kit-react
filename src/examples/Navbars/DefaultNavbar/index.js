d78048b7f3bd5a947ef2ca41e0a6a49426f7435b
utkarsh <usrivastava92@gmail.com>
2023-06-29 23:27:04 +0530

    added env file

--- src/examples/Navbars/DefaultNavbar/index.js
+++ src/examples/Navbars/DefaultNavbar/index.js
@@ -551,7 +551,7 @@ function DefaultNavbar({ brand, routes, transparent, light, action, sticky, rela
 
 // Setting default values for the props of DefaultNavbar
 DefaultNavbar.defaultProps = {
-  brand: "Material Kit 2",
+  brand: process.env.REACT_APP_TITLE,
   transparent: false,
   light: false,
   action: false,
