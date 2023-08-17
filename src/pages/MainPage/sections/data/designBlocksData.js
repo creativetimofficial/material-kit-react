b7b18d235ea8f702b74ce649c74e8ac02d8234ff
April Bates <aprilb@aleios.com>
2023-08-17 12:19:53 +0100

    feat: removing unhelpful design blocks

--- src/pages/MainPage/sections/data/designBlocksData.js
+++ src/pages/MainPage/sections/data/designBlocksData.js
@@ -18,78 +18,6 @@ export default [
         count: 14,
         route: "/sections/page-sections/features",
       },
-      {
-        image: `${imagesPrefix}/pricing.jpg`,
-        name: "Pricing",
-        count: 8,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/faq.jpg`,
-        name: "FAQ",
-        count: 1,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/blogs.jpg`,
-        name: "Blog Posts",
-        count: 11,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/testimonials.jpg`,
-        name: "Testimonials",
-        count: 11,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/teams.jpg`,
-        name: "Teams",
-        count: 6,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/stats.jpg`,
-        name: "Stats",
-        count: 3,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/call-to-action.jpg`,
-        name: "Call to Actions",
-        count: 8,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/projects.jpg`,
-        name: "Applications",
-        count: 6,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/logo-area.jpg`,
-        name: "Logo Areas",
-        count: 4,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/footers.jpg`,
-        name: "Footers",
-        count: 10,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/general-cards.jpg`,
-        name: "General Cards",
-        count: 9,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/content-sections.jpg`,
-        name: "Content Sections",
-        count: 8,
-        pro: true,
-      },
     ],
   },
   {
@@ -121,18 +49,6 @@ export default [
     description: "50+ elements that you need for text manipulation and insertion",
     items: [
       {
-        image: `${imagesPrefix}/newsletters.jpg`,
-        name: "Newsletters",
-        count: 6,
-        pro: true,
-      },
-      {
-        image: `${imagesPrefix}/contact-sections.jpg`,
-        name: "Contact Sections",
-        count: 8,
-        pro: true,
-      },
-      {
         image: `${imagesPrefix}/forms.jpg`,
         name: "Forms",
         count: 3,
@@ -155,12 +71,6 @@ export default [
         name: "Alerts",
         count: 4,
         route: "/sections/attention-catchers/alerts",
-      },
-      {
-        image: `${imagesPrefix}/toasts.jpg`,
-        name: "Notifications",
-        count: 3,
-        pro: true,
       },
       {
         image: `${imagesPrefix}/popovers.jpg`,
@@ -205,12 +115,6 @@ export default [
         route: "/sections/elements/toggles",
       },
       {
-        image: `${imagesPrefix}/social-buttons.jpg`,
-        name: "Social Buttons",
-        count: 2,
-        pro: true,
-      },
-      {
         image: `${imagesPrefix}/breadcrumbs.jpg`,
         name: "Breadcrumbs",
         count: 1,
@@ -227,12 +131,6 @@ export default [
         name: "Progress Bars",
         count: 4,
         route: "/sections/elements/progress-bars",
-      },
-      {
-        image: `${imagesPrefix}/tables.jpg`,
-        name: "Tables",
-        count: 3,
-        pro: true,
       },
       {
         image: `${imagesPrefix}/typography.jpg`,
