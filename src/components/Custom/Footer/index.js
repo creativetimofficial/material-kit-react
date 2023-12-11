31bf27c0977c09f02f14bfb227e22b359a83a531
Guillermo Reyes <guillermo_hard@hotmail.com>
2023-12-11 16:26:57 -0600

    add index

---
+++ src/components/Custom/Footer/index.js
@@ -0,0 +1,118 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// react-router-dom components
+import { Link } from "react-router-dom";
+
+// prop-types is a library for typechecking of props.
+import PropTypes from "prop-types";
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+function DefaultFooter({ content }) {
+  const { brand, socials, menus, copyright } = content;
+
+  return (
+    <MKBox component="footer">
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={3} sx={{ ml: "auto", mb: 3 }}>
+            <MKBox>
+              <Link to={brand.route}>
+                <MKBox component="img" src={brand.image} alt={brand.name} maxWidth="2rem" mb={2} />
+              </Link>
+              <MKTypography variant="h6">{brand.name}</MKTypography>
+            </MKBox>
+            <MKBox display="flex" alignItems="center" mt={3}>
+              {socials.map(({ icon, link }, key) => (
+                <MKTypography
+                  key={link}
+                  component="a"
+                  href={link}
+                  target="_blank"
+                  rel="noreferrer"
+                  variant="h5"
+                  color="dark"
+                  opacity={0.8}
+                  mr={key === socials.length - 1 ? 0 : 2.5}
+                >
+                  {icon}
+                </MKTypography>
+              ))}
+            </MKBox>
+          </Grid>
+          {menus.map(({ name: title, items }) => (
+            <Grid key={title} item xs={6} md={2} sx={{ mb: 3 }}>
+              <MKTypography
+                display="block"
+                variant="button"
+                fontWeight="bold"
+                textTransform="capitalize"
+                mb={1}
+              >
+                {title}
+              </MKTypography>
+              <MKBox component="ul" p={0} m={0} sx={{ listStyle: "none" }}>
+                {items.map(({ name, route, href }) => (
+                  <MKBox key={name} component="li" p={0} m={0} lineHeight={1.25}>
+                    {href ? (
+                      <MKTypography
+                        component="a"
+                        href={href}
+                        target="_blank"
+                        rel="noreferrer"
+                        variant="button"
+                        fontWeight="regular"
+                        textTransform="capitalize"
+                      >
+                        {name}
+                      </MKTypography>
+                    ) : (
+                      <MKTypography
+                        component={Link}
+                        to={route}
+                        variant="button"
+                        fontWeight="regular"
+                        textTransform="capitalize"
+                      >
+                        {name}
+                      </MKTypography>
+                    )}
+                  </MKBox>
+                ))}
+              </MKBox>
+            </Grid>
+          ))}
+          <Grid item xs={12} sx={{ textAlign: "center", my: 3 }}>
+            {copyright}
+          </Grid>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+// Typechecking props for the DefaultFooter
+DefaultFooter.propTypes = {
+  content: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.object, PropTypes.array])).isRequired,
+};
+
+export default DefaultFooter;
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,11 +1,66 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+import MKSocialButton from "components/MKSocialButton";
+import Navbar from "components/Custom/Navbar"
+import Footer from "components/Custom/Footer"
+
+// Routes
+import routes from "routes";
+import footerRoutes from "footer.routes";
 
 function Index() {
-    return (
-      <>
-      <p>HOLA MUNDO</p>
-      </>
-    )
-}
+  return (
+    <>
+      <Navbar
+        routes={routes}
+        sticky
+      />
+      <MKBox
+        minHeight="100vh"
+        width="100%"
+        sx={{
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+            <p>Conoce personas</p>
+            <button>Registrate</button><br/>
+            <button>Inicia sesion</button>
 
+            <p>Instala la app</p>
+            <button>google play</button>
+        </Container>
+        </MKBox>
+      
+      <MKBox pt={6} px={1} mt={6}>
+        <Footer content={footerRoutes} />
+      </MKBox>
+    </>
+  );
+}
 
 export default Index;
