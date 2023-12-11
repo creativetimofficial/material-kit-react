c8460ecca1a6d186039e37d92bf58652e1f77cb8
Guillermo Reyes <guillermo_hard@hotmail.com>
2023-12-11 15:47:52 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -56,8 +56,8 @@ export default function App() {
       <CssBaseline />
       <Routes>
         {getRoutes(routes)}
-        <Route path="/presentation" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route path="/" element={<Presentation />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </ThemeProvider>
   );
