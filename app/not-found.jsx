695f5c37ee13adf3e9d20bd5674d865b98383e06
Guillermo Reyes <guillermo_hard@hotmail.com>
2023-12-12 23:30:22 -0600

    add navbar

---
+++ app/not-found.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar(){
+    return (
+        <div>
+            ups
+        </div>
+    )
+}
--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -1,17 +1,35 @@
 import Link from "next/link";
 import "./styles.css"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import InstallMobileIcon from '@mui/icons-material/InstallMobile';
+import InfoIcon from '@mui/icons-material/Info';
 
 export default function Navbar(){
     return (
-        <nav className="navbar">
-            <Link href={"/"}>
-                <h1>CodeMate</h1>
-            </Link>
-            <ul>
-                <li>
-                    <Link href="/">hola</Link>
-                </li>
-            </ul>
+      <>
+        <nav className="fixed top-0 w-full bg-red-500 p-4 z-10 text-white">
+          <div className="container mx-auto flex items-center justify-between">
+            <div className=" text-xl font-bold">
+                <Link href="/">
+                    Logo
+                </Link>
+            </div>
+            <div className="flex space-x-4">
+              <Link href="/">
+                <AccountCircleIcon />
+                Inicia sesión
+              </Link>
+              <Link href="/about">
+                <InfoIcon />
+                Acerca de
+              </Link>
+              <Link href="/contact">
+                <InstallMobileIcon />
+                Instala la app
+              </Link>
+            </div>
+          </div>
         </nav>
+      </>
     );
 }
