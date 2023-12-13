87651faa9d37595452952f9550fb8db685f4ffe6
Guillermo Reyes <guillermo_hard@hotmail.com>
2023-12-13 00:29:15 -0600

    add landing

--- app/page.jsx
+++ app/page.jsx
@@ -1,53 +1,8 @@
+import LandingVideo from "@/components/landingvideo";
 export default function HomePage(){
     return (
-    <div>
-        <p>hola mundo 1</p>
-        <p>hola mundo 2</p>
-        <p>hola mundo 3</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-        <p>hola mundo</p>
-    </div>
-    )
+        <div>
+            <LandingVideo />
+        </div>
+    );
 }
