50090a68118e5a53a983b9a6e62e38d18ca526ad
Guillermo Reyes <guillermo_hard@hotmail.com>
2023-12-14 14:36:03 -0600

    edit activities

--- app/view/activities/page.jsx
+++ app/view/activities/page.jsx
@@ -1,76 +1,43 @@
 "use client"
-import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
-import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
-import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-import MessageRoundedIcon from '@mui/icons-material/MessageRounded';
-import Link from 'next/link';
+import React, { useState } from 'react';
 
-const PendingButtons = ({id}) => {
+const ContainerComponent = () => {
+  const itemsRow1 = [...Array(100).keys()]; // Puedes cambiar la cantidad de elementos según tus necesidades
+  const itemsCol1 = [...Array(100).keys()]; // Puedes cambiar la cantidad de elementos según tus necesidades
+
+  const [scrollLeft, setScrollLeft] = useState(0);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const handleScroll = (e) => {
+    setScrollLeft(e.target.scrollLeft);
+    setScrollTop(e.target.scrollTop);
+  };
+
   return (
     <>
-      <button className="bg-red-500 text-white px-4 py-2 rounded-md mr-2">
-        <CancelRoundedIcon />
-      </button>
-      <Link href={'/view/profile/'+id} className="bg-slate-400 text-white px-4 py-2 rounded-md mr-2">
-        <InfoRoundedIcon />
-      </Link>
-      <button className="bg-green-500 text-white px-4 py-2 rounded-md">
-        <CheckCircleRoundedIcon />
-      </button>
-    </>
-  );
-}
-
-const ApprovedButtons = ({id}) => {
-  return (
-    <>
-      <button className="bg-red-500 text-white px-4 py-2 rounded-md mr-2">
-        <CancelRoundedIcon />
-      </button>
-      <Link href={'/view/profile/'+id} className="bg-slate-400 text-white px-4 py-2 rounded-md mr-2">
-        <InfoRoundedIcon />
-      </Link>
-      <Link  href={'/view/messages/'+id} className="bg-cyan-600 text-white px-4 py-2 rounded-md mr-2">
-        <MessageRoundedIcon />
-      </Link>
-    </>
-  );
-}
-
-const Buttons = ({type, id}) => {
-  return (
-    <div className="flex justify-between">
-        {type == "PENDING"? <PendingButtons id={id} />: <ApprovedButtons id={id} />}
+      {/*LISTA DE MATCHES*/}
+      <div className="flex items-center overflow-x-auto">
+        {itemsRow1.map((item) => (
+          <div key={item} className="w-64 h-32 bg-gray-300 m-2">
+            perfil {item}
+          </div>
+        ))}
       </div>
-  )
-}
 
-const Card = ({ photoUrl, name, onReject, onApprove, type, userid }) => {
-  return (
-    <div className="bg-white p-4 rounded-lg shadow-md">
-      <img src={photoUrl} alt={name} className="w-full h-48 object-cover mb-4 rounded-lg" />
-      <h2 className="text-black text-xl font-semibold mb-2">{name}</h2>
-      <Buttons type={type} id={userid}/>
-    </div>
+      <div className="border flex flex-grow max-h-screen">
+        {/*Lista de mensajes*/}
+        <div className="w-1/4 overflow-y-auto">
+          {itemsCol1.map((item) => (
+            <div key={item} className="w-full h-16 bg-gray-300 mb-2">
+              mensaje {item}
+            </div>
+          ))}
+        </div>
+        {/*Contenido del mensaje*/}
+        <div className="flex-grow overflow-y-auto">CONTENIDO</div>
+      </div>
+    </>
   );
 };
 
-
-export default function CardCollection (params){
-    const cards = [
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"PENDING", "userid": "ojaosjoash"},
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"APPROVED", "userid": "a4s56d45a4d"},
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"PENDING", "userid": "78a7d8s97d9a"},
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"PENDING", "userid": "s54da4d5as4"},
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"APPROVED", "userid": "d87s98fg7d"},
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"APPROVED", "userid": "zc2x13zc1z2"},
-      {"photoUrl":"/profile.jpg", "name":"Pedro - 26 años", "type":"APPROVED", "userid": "7w9q79qwe7"},
-    ]
-    return (
-      <div className="container max-w-4xl mx-auto mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {cards.map((card, index) => (
-          <Card key={index} {...card} />
-        ))}
-      </div>
-    );
-  };
+export default ContainerComponent;
