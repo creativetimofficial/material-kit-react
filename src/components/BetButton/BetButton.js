8234dd8d12f1ddb7a0af3b1ce332eec26837d22c
HarlyPonari <harly.ponari@stu.bcc.cuny.edu>
2024-04-03 18:39:55 -0400

    Working on transaction implementation

--- src/components/BetButton/BetButton.js
+++ src/components/BetButton/BetButton.js
@@ -1,83 +1,55 @@
 import React from "react";
 import MKButton from "components/MKButton";
-import { useAnchorWallet } from "@solana/wallet-adapter-react";
+import { useAnchorWallet, useWallet } from "@solana/wallet-adapter-react";
 import { Program, AnchorProvider, web3, BN } from "@project-serum/anchor";
-import { Connection } from "@solana/web3.js";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
 import idl from "../../idl.json";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { decode } from "@project-serum/anchor/dist/cjs/utils/bytes/hex";
+import * as buffer from "buffer";
+import { bs58 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import { Key } from "@mui/icons-material";
+
+
+window.Buffer = buffer.Buffer;
 
 const BetButton = ({ amount }) => {
-  const wallet = useAnchorWallet();
+
+  const wallet = useWallet();
   const baseAccount = web3.Keypair.generate();
 
-  function getProvider() {
-    if (!wallet) {
-      return null;
-    }
-    const network = "http://192.168.1.197:8899";
-    const connection = new Connection(network, "processed");
-
-    const provider = new AnchorProvider(connection, wallet, { preflightCommitment: "processed" });
-    return provider;
-  }
-
-  async function createCounter() {
-    const provider = getProvider();
-    if (!provider) {
-      throw "Provider is null";
-    }
-    const a = JSON.stringify(idl);
-    const b = JSON.parse(a);
-    const program = new Program(b, idl.metadata.address, provider);
-    try {
-      await program.methods
-        .initialize()
-        .accounts({
-          myAccount: baseAccount.publicKey,
-          user: provider.wallet.publicKey,
-          systemProgram: web3.SystemProgram.programId,
-        })
-        .signers([baseAccount])
-        .rpc();
-
-      const account = await program.account.myAccount.fetch(baseAccount.publicKey);
-      console.log("Account: ", account);
-    } catch (err) {
-      console.log("transaction error", err);
-    }
-  }
-
-  async function increment() {
-    const provider = getProvider();
-    if (!provider) {
-      throw "Provider is null";
+  async function createTransaction() {
+    if (!wallet.publicKey) {
+      console.log("Could not get the public key")
+      return
     }
 
-    const a = JSON.stringify(idl);
-    const b = JSON.parse(a);
-    const program = new Program(b, idl.metadata.address, provider);
-    try {
-      await program.methods
-        .increment()
-        .accounts({
-          myAccount: baseAccount.publicKey,
-        })
-        .rpc();
+    const privateKey = new Uint8Array(bs58.decode(process.env['PRIVATE_KEY']))
 
-      const account = await program.account.myAccount.fetch(baseAccount.publicKey);
-      console.log("Account: ", account.data.toString());
-    } catch (err) {
-      console.log("transaction error", err);
-    }
-  }
+    const sender = Keypair.fromSecretKey(privateKey)
+
+    const reciever = Keypair.generate()
+
+    const connection = new Connection(clusterApiUrl('devnet'), "processed")
+
+      (async () => {
+        const tx = new Transaction().add(
+          SystemProgram.transfer({
+            fromPubkey: sender.publicKey,
+            toPubkey: reciever.publicKey,
+            lamports: LAMPORTS_PER_SOL * 0.001,
+          })
+        )
+        const signature = await sendAndConfirmTransaction(connection, tx, [sender])
+
+      } 
 
   return (
-    <React.Fragment>
-      <button onClick={createCounter}>Create Counter</button>
-      <button onClick={increment}>Increment</button>
-      <WalletMultiButton />
-    </React.Fragment>
-   );
-};
+      <React.Fragment>
+        <MKButton onClick={createTransaction}>Send Money!!!</MKButton>
+        <WalletMultiButton />
+      </React.Fragment>
+    );
+  }
 
-export default BetButton;
+  export default BetButton;
