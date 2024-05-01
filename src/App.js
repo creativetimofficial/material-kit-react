1f566f1345651c2ba25748a6821187d4ddd2625d
Oleh Smolikevych <olehsjs@gmail.com>
2024-04-30 19:24:59 -0500

    Head content added before cleaning the project from unused components

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 
 // Material Kit 2 React themes
 import theme from "assets/theme";
-import Presentation from "layouts/pages/presentation";
+import Home from "layouts/pages/home";
 
 // Material Kit 2 React routes
 import routes from "routes";
@@ -56,8 +56,8 @@ export default function App() {
       <CssBaseline />
       <Routes>
         {getRoutes(routes)}
-        <Route path="/presentation" element={<Presentation />} />
-        <Route path="*" element={<Navigate to="/presentation" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="*" element={<Navigate to="/home" />} />
       </Routes>
     </ThemeProvider>
   );
