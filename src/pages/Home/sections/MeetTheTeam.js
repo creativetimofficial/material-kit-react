0bb8598f7a02f6de4611bab94e6361cd56010842
Brady Cusack <95650239+bcusack1996@users.noreply.github.com>
2024-05-13 20:03:53 -0500

    added alicia to meet the team, added photo, rearranged team members

--- src/pages/Home/sections/MeetTheTeam.js
+++ src/pages/Home/sections/MeetTheTeam.js
@@ -18,6 +18,8 @@ import { styled } from "@mui/material/styles";
 import BradyImg from "../../../assets/images/Brady.jpeg";
 import ChrisImg from "../../../assets/images/Chris.jpeg";
 import RichardImg from "../../../assets/images/Richard.jpg";
+import AliciaImg from "../../../assets/images/aliciaauman.jpeg";
+
 
 const MemberCard = styled(Paper)(({ theme, layout }) => ({
   display: "flex",
@@ -48,22 +50,28 @@ const Image = styled("img")({
 function MeetTheTeam() {
   const teamMembers = [
     {
-      name: "Brady Cusack",
-      position: "position",
-      bio: "Brady Cusack is an experienced technology leader with expertise in enterprise architecture, robotics process automation, and cybersecurity operations. Brady graduated from Towson University with a B.S. in Computer Science focused on Cybersecurity. He then worked as an automation consultant for numerous small businesses where he helped lead implementations at Fortune 500 companies. Brady later founded a small managed service provider, growing it to support two federal agencies and five government contractors.",
-      imageUrl: BradyImg,
+      name: "Richard Gordon",
+      position: "Chairman",
+      bio: "Richard Gordon utilizes a comprehensive array of resources to enhance customer agility, speed, and evidence-based decision-making, allowing for preemptive problem solving. His varied experience which ranges from Capital Hill to Executive Leadership consulting equips him uniquely to contribute significantly to your success.",
+      imageUrl: RichardImg,
     },
     {
       name: "Chris Leiter",
-      position: "position",
+      position: "SVP of Operations",
       bio: "Chris Leiter is a seasoned professional in RPA, cybersecurity, and IT solutions. He has experience in both the government and private sectors. Prior to cofounding Poe Security, Chris held a pivotal role at Blue Prism as a Senior Solutions Engineer/Architect, leading the integration of RPA in government operations. His responsibilities included introducing innovative RPA solutions, providing training, and crafting Proof of Concept solutions. Chris has over 11 years of experience in IT and cybersecurity in government contracting, starting in the Army Infantry and transitioning to armed security at the Department of State. He holds CISSP, Security+, and DoD 8570.1 M IAM level III certifications, with expertise across networks including the Department of State, Department of Defense, and Department of Justice. Chris's diverse exposure has honed his technical skills and solidified his passion for security and information assurance.",
       imageUrl: ChrisImg,
     },
     {
-      name: "Richard Gordon",
-      position: "position",
-      bio: "Richard Gordon utilizes a comprehensive array of resources to enhance customer agility, speed, and evidence-based decision-making, allowing for preemptive problem solving. His varied experience which ranges from Capital Hill to Executive Leadership consulting equips him uniquely to contribute significantly to your success.",
-      imageUrl: RichardImg,
+      name: "Alicia Auman",
+      position: "Federal Sales Lead",
+      bio: "Alicia Auman joins G&L Federal bringing experience selling software to state and local government entites. Alicia has over a decade of experience in the education space, spending years working as a resource inside and out of the classroom.",
+      imageUrl: AliciaImg,
+    },
+    {
+      name: "Brady Cusack",
+      position: "SVP Technology",
+      bio: "Brady Cusack is an experienced technology leader with expertise in enterprise architecture, robotics process automation, and cybersecurity operations. Brady graduated from Towson University with a B.S. in Computer Science focused on Cybersecurity. He then worked as an automation consultant for numerous small businesses where he helped lead implementations at Fortune 500 companies. Brady later founded a small managed service provider, growing it to support two federal agencies and five government contractors.",
+      imageUrl: BradyImg,
     },
   ];
 
